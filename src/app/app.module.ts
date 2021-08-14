844f1939c7a80cae68f22f1abeb3f2eabd647304
kevin <kbarriosc8@miumg.edu.com>
2021-08-14 14:01:53 -0600

    Implementacion de contenido de imagenes lado derecho de la aplicacion web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { SIDEBARComponent } from './sidebar/sidebar.component';
+import { ImagencentralComponent } from './imagencentral/imagencentral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SIDEBARComponent,
+    ImagencentralComponent
   ],
   imports: [
     BrowserModule,
